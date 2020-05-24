c1178f8e2c578e6bc90026dd11b8c8ce1d39364d
Machu Rexford <[rexfordmachu@gmail.com]>
2020-05-24 13:37:30 +0000

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Rexford Machu on 5/24/20.
+//  Copyright © 2020 MachuDigital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
